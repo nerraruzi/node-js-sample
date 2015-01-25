9a7527df690d83bd2891f6c7f975fb80c2b53712
Fan Pu <zengfanpu@gmail.com>
2015-01-25 03:21:05 +0000

    node returns index.html code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express')
 var app = express();
-
+var fs = require('fs');
+var callback = function(err, contents){
+    response.write(contents);
+    response.end();
+}
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('TravelNet-Work in Progress ')
+    fs.readFile('index.html',callback);
+    response.send('TravelNet-Work in Progress ')
 })
 
 app.listen(app.get('port'), function() {
